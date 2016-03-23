514fb5200f7be41eea7bd4b3351aebcbdc73cb44
PavlivAndriy <andriypavliv1@gmail.com>
2016-03-23 23:04:57 +0200

    Added 2 .sql files, changed statements, changed .gitignore and pom.xml

--- src/main/java/org/dentist/user/service/DatabaseConnector.java
+++ src/main/java/org/dentist/user/service/DatabaseConnector.java
@@ -5,62 +5,61 @@ import org.dentist.user.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DatabaseConnector {
     private static final Logger logger = LoggerFactory.getLogger(DatabaseConnector.class);
     private DatabaseWorker databaseWorker = new DatabaseWorker();
-    private ResultSet resultSet;
-    private Statement statement;
+    private ResultSet resultSet = null;
+    private PreparedStatement preparedStatement;
+    private final static String INSERT_NEW = "INSERT INTO users VALUES (?,?,?,?,?)";
+    private final static String GET_ALL = "SELECT * FROM users";
+
+    private void addNewRows(){
+        try {
+            preparedStatement = databaseWorker.getConnection().prepareStatement(INSERT_NEW);
+            preparedStatement.setInt(1, 8);
+            preparedStatement.setString(2, "Jonatan");
+            preparedStatement.setString(3, "Jonn");
+            preparedStatement.setString(4, "jonatan@gmail.com");
+            preparedStatement.setString(5, "09566633322");
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            logger.error("You received exception " + e);
+        }
+    }
 
     private void checkConnection() {
-        String query = "select * from users";
         try {
-            statement = databaseWorker.getConnection().createStatement();
-            resultSet = statement.executeQuery(query);
+            preparedStatement = databaseWorker.getConnection().prepareStatement(GET_ALL);
+            resultSet = preparedStatement.executeQuery();
         } catch (SQLException e) {
             logger.error("You received exception " + e);
         }
     }
 
-    private void fillDatabase(){
-        try {
-            statement.execute("INSERT INTO users(id, name, nickname, email, phone)" +
-                    "VALUES (5, 'Tom', 'Tom3', 'tom@gmail.com', '0990909900')");
-            statement.execute("INSERT INTO users(id, name, nickname, email, phone)" +
-                    "VALUES (6, 'Joe', 'Joe2', 'joe@gmail.com', '0887862767')");
-            statement.execute("INSERT INTO users(id, name, nickname, email, phone)" +
-                    "VALUES (7, 'Oliver', 'Oliver2', 'oliver@gmail.com','0998465488')");
-        } catch (SQLException e) {
-            logger.error("You received exception " + e);
-        } finally {
-            try {
-                statement.close();
-            } catch (SQLException e) {
-                logger.error("You received exception " + e);
-            }
-        }
-    }
 
     public void printDatabase() {
+        addNewRows();
         checkConnection();
-        /*fillDatabase();*/
-        try {
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getInt("id"));
-                user.setName(resultSet.getString("name"));
-                user.setNickname(resultSet.getString("nickname"));
-                user.setEmail(resultSet.getString("email"));
-                user.setPhone(resultSet.getString("phone"));
-                System.out.println(user);
+        if (resultSet != null) {
+            try {
+                while (resultSet.next()) {
+                    User user = new User();
+                    user.setId(resultSet.getInt("id"));
+                    user.setName(resultSet.getString("name"));
+                    user.setNickname(resultSet.getString("nickname"));
+                    user.setEmail(resultSet.getString("email"));
+                    user.setPhone(resultSet.getString("phone"));
+                    System.out.println(user);
+                }
+            } catch (SQLException e) {
+                logger.error("You received exception " + e);
+            } catch (NullPointerException e) {
+                logger.error("You received exception " + e);
             }
-        } catch (SQLException e) {
-            logger.error("You received exception " + e);
-        } catch (NullPointerException e) {
-            logger.error("You received exception " + e);
         }
     }
 
